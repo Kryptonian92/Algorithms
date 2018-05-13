bdd8d48157a59787e23393d5fa9a2d394d0ec851
Ausar McGruder <ausar_mcgruder@yahoo.com>
2018-05-12 23:01:41 -0400

    Daily python algos

---
+++ PythonAlgos/Basic13/minMaxAverage.py
@@ -0,0 +1,18 @@
+#Finds the min, max and average
+import math
+from decimal import *
+
+numArray = [1,5,7,10,3];
+total = 0;
+maxNum = 0;
+minNum = 0;
+def arrValues(arr,t,m,l):
+    for i in range (len(arr)):
+        t+=arr[i];
+        if(arr[i]>m):
+            m=arr[i]
+        if(arr[i]<arr[i]+1):
+            l=arr[i]
+    print ("Max: "+m+"Minimum: "+l+"Average: " + (t/len(arr)))
+    # print ("The average is " + "%.2f" % (3/2))
+arrValues(numArray,total,maxNum,minNum)
