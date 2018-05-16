af295ecc6fc61b7af36cb5e615c01425536c4f0c
Ausar McGruder <ausar_mcgruder@yahoo.com>
2018-05-16 19:35:57 -0400

    Python parser algo

---
+++ PythonAlgos/listParser.py
@@ -0,0 +1,15 @@
+
+# listValues = [];
+listValues = [4,6,6,8,4,4,4,7,7,8];
+
+maxMode = 0;
+
+def parser(l,m):
+    for i in range(10):
+        l.append(i)
+    print(l)
+    for i in (len(l)):
+        for j in (j[i],len(l))
+            if(l[i]==):
+
+parser(listValues,maxMode)
