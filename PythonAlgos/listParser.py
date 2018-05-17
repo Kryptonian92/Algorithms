5f424c9a6b24df0c0bafac4a1eecf386dca3eba5
Ausar McGruder <ausar_mcgruder@yahoo.com>
2018-05-17 11:33:07 -0400

    Python parsing algo

--- PythonAlgos/listParser.py
+++ PythonAlgos/listParser.py
@@ -1,15 +1,35 @@
 
 # listValues = [];
-listValues = [4,6,6,8,4,4,4,7,7,8];
+listValues = [4,4,4,4,4,4,4,4,4,4];
 
 maxMode = 0;
+counter = 0;
+maxNum = 0;
 
-def parser(l,m):
-    for i in range(10):
-        l.append(i)
-    print(l)
-    for i in (len(l)):
-        for j in (j[i],len(l))
-            if(l[i]==):
+# def parser(l,m,c,mn):
+#     for i in range(10):
+#         l.append(i)
+#     print(l)
+#     for i in range (len(l)):
+#         # print ("outer loop loop")
+#         for j in range (i,len(l)):
+#             # print("inner loop")
+#             # print(l[i])
+#             # print(l[j])
+#             if(l[i]==l[j]):
+#                 c+=1;
+#             else:
+#                 print("not counting")
+#     print(counter);
+#
+# parser(listValues,maxMode,counter,maxNum)
 
-parser(listValues,maxMode)
+for i in range (len(listValues)):
+    for j in range (i,len(listValues)):
+        if(listValues[i]==listValues[j]):
+            counter+=1;
+            print("Working")
+        else:
+            print("Not working")
+print (listValues[i]);
+print (counter)
